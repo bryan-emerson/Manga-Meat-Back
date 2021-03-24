89072bf4805c94c33906b51cea3cd1b8ac343422
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-24 18:46:56 -0400

    src/app.js instantiated

---
+++ src/app.js
@@ -0,0 +1,13 @@
+let express = require('express')
+let router = require('./router')
+
+let cors = require('cors')
+let app = express()
+
+
+
+app.use(express.json())
+app.use(cors())
+app.use(router)
+
+module.exports = app
