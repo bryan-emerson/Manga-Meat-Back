1f5e88b41946467a15cfeef88b3e50838ca61dcc
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-24 19:01:02 -0400

    database.js instantiated connection string needs attention

---
+++ src/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true }
+
+let connectionString = ""
+if(process.env.NODE_ENV === "production"){
+  connectionString = process.env.DB_URL
+} else {
+  connectionString = "mongodb://localhost/********" //this string needs to be adjusted
+}
+
+mongoose.connect(connectionString, mongooseConnectionConfig);
+
+
+
+mongoose.connection.on('connected', ()=> console.log("connected to database"))
+mongoose.connection.on('disconnected', ()=> console.log("disconnected from database"))
+mongoose.connection.on('error', error=> console.error("Database error", error))
+module.exports = mongoose;
