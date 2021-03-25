8ac0536114dee9dc3f2e58e0bbff9b56dfe00469
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-25 11:49:37 -0400

    seed.js instantiated for food chars and posts

---
+++ src/seed.js
@@ -0,0 +1,29 @@
+require('./database.js')
+let mongoose = require('mongoose')
+
+let foodJson = require('./data/food.json')
+let AnimeFood = require('./mangaMeat/models/food.js')
+
+let characterJson = require('./data/character.json')
+let AnimeChar = require('./mangaMeat/models/character.js')
+
+let postJson = require('./data/posts.json')
+let Posts = require('./mangaMeat/models/posts.js')
+
+AnimeFood
+  .deleteMany({})
+  .then(()=> AnimeFood.create(foodJson))
+  .then(mongoose.disconnect)
+  .then(()=> console.log('AnimeChar seeding done!'))
+
+AnimeChar
+  .deleteMany({})
+  .then(()=> AnimeChar.create(characterJson))
+  .then(mongoose.disconnect)
+  .then(()=> console.log('AnimeChar seeding done!'))
+
+Posts
+  .deleteMany({})
+  .then(()=> Posts.create(postJson))
+  .then(mongoose.disconnect)
+  .then(()=> console.log('AnimeChar seeding done!'))
